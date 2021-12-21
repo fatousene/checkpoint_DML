2210dab1a4bd366b267af8cd7ed0274f9292bb8a
fatousene <sene0299@gmail.com>
2021-12-21 12:57:15 +0000

    first commit

---
+++ mdl.sql
@@ -0,0 +1,12 @@
+INSERT INTO Produit (Product_id, Product_name, Category, Price)
+VALUES ('P01', 'Samsung Galaxy S20', 'Smarphone', 3299);
+INSERT INTO Produit (Product_id, Product_name, Category, Price)
+VALUES ('P02', 'ASUS Notebook', 'PC', 4599);
+
+INSERT INTO Customer VALUES('C01', 'ALY', 71321009)
+INSERT INTO Customer VALUES('C02', 'ASMA', 77345823)
+ 
+INSERT INTO Produit (Customer_id, Product_id, Quantity, Total_amount)
+VALUES ('C01', 'P02', NULL, '2', 9198);
+INSERT INTO Produit (Customer_id, Product_id, Quantity, Total_amount)
+VALUES ('C02', 'P01', 28/05/2020, '1', 3299);
